cc19719c7367982cf821de93a27dab2c2e4e03f4
Enzo Rodrigues <167582668+EnzoRodrigues9@users.noreply.github.com>
2025-10-28 22:21:41 -0300

    Delete ToonArchive directory

--- ToonArchive/backend/src/main/java/PI/backend/repositories/MangaRepository.java
+++
@@ -1,7 +0,0 @@
-package PI.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import PI.backend.models.Manga;
-
-public interface MangaRepository extends JpaRepository<Manga, Long> {
-}
